4517df9719497bfdd7e54ed7d7f5356e7ef04784
Willy Wonka <Woodenehad9798740@yandex.ru>
2023-01-12 11:47:46 +0300

    add admin for mainapp

--- project/mainapp/admin.py
+++ project/mainapp/admin.py
@@ -1,1 +1,23 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Courses, CourseTeachers, Lesson, News
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Courses)
+class CoursesAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(CourseTeachers)
+class CourseTeachersAdmin(admin.ModelAdmin):
+    pass
