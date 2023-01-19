1eb3833741ea1145644d8d237f5e95f934c819b7
Willy Wonka <Woodenehad9798740@yandex.ru>
2023-01-19 18:34:20 +0300

    mainapp: Views & Templates | template_tags

--- project/config/settings.py
+++ project/config/settings.py
@@ -10,11 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -55,7 +56,9 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            os.path.join(SETTINGS_PATH, "templates"),
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- project/mainapp/urls.py
+++ project/mainapp/urls.py
@@ -4,11 +4,13 @@ from mainapp.apps import MainappConfig
 
 app_name = MainappConfig.name
 
+# http://127.0.0.1:8000/mainapp/...
 urlpatterns = [
-    path("", views.MainPageView.as_view()),
-    path("news/", views.NewsPageView.as_view()),
-    path("courses/", views.CoursesPageView.as_view()),
-    path("contacts/", views.ContactsPageView.as_view()),
-    path("doc_site/", views.DocSitePageView.as_view()),
-    path("login/", views.LoginPageView.as_view()),
+    path("", views.MainPageView.as_view(), name="index"),
+    path("news/", views.NewsPageView.as_view(), name="news"),
+    path("news/<int:page>/", views.NewsWithPaginatorView.as_view(), name="news_paginator"),
+    path("courses/", views.CoursesPageView.as_view(), name="courses_list"),
+    path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
+    path("doc_site/", views.DocSitePageView.as_view(), name="docs"),
+    path("login/", views.LoginPageView.as_view(), name="login"),
 ]
